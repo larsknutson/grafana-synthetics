8b4306735c9d87d4e61609afebd28f1d90852947
Lars Knutson <knutson.lars@gmail.com>
2026-04-21 14:17:29 +0200

    adding grafana alerts

--- terraform/synthetics/providers.tf
+++ terraform/synthetics/providers.tf
@@ -1,5 +1,5 @@
 provider "grafana" {
-  url             = "https://larsknutson-devmio.grafana.net/"
+  url             = "https://larsknutson.grafana.net/"
   auth            = var.grafana_service_token
   sm_url          = "https://synthetic-monitoring-api-eu-west-2.grafana.net"
   sm_access_token = var.sm_access_token
